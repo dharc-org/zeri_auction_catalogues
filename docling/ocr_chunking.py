f3cdecc78e2e8631bfbe4fd07d746f95d2936f14
marilena <marilena.daquino2@unibo.it>
2026-02-16 09:44:09 +0100

    fix and merge

--- docling/ocr_chunking.py
+++ docling/ocr_chunking.py
@@ -14,6 +14,11 @@ import time
 from collections import defaultdict
 import sys
 
+# FUTURE REUSER: modify the two hardcoded URIs for images
+## var IIIF_SEARCH_URL: the URL of the IIIF server search api to select pages
+## def find_markdown_file > var page_uri: rebuilds the URL of the IIIF image associated to a chunk
+
+
 #IIIF_SEARCH_URL = "http://137.204.64.39/presentation/iiif/search?q=collection_id=LOTTO1;classification=Item+Description;is_table=0"
 IIIF_SEARCH_URL = "http://137.204.64.39/presentation/iiif/search?q=filename=BO0624_81777;collection_id=LOTTO1;classification=Item+Description;is_table=0"
 
@@ -133,7 +138,7 @@ def get_image(chunks_df, input_folder, catalogue_id):
         text_clean = str(text).strip()
         for fname, content in markdown_files.items():
             if text_clean in content:
-                page_uri = c.iiif_page_uri_base + catalogue_id + '!' + urllib.parse.quote(fname).replace("documents/","") + '/full/max/0/default.jpg'
+                page_uri = c.iiif_page_uri_base + catalogue_id.replace("documents/","") + '!' + urllib.parse.quote(fname[:-3].replace("documents/","")) + '.jpg/full/max/0/default.jpg'
                 print(page_uri)
                 return page_uri
         return None
